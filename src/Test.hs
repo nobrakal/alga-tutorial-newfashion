7c8a979a36a9dd32ded10be1c144d45c6e9e5f44
Alexandre Moine <alexandre@moine.me>
2018-06-07 22:20:10 +0200

    Update mod4

--- src/Test.hs
+++ src/Test.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 module Main where
 
 import Data.Foldable (forM_)
@@ -16,11 +14,11 @@ import Eval
 import Types
 
 import qualified Data.Text as T
-
+import qualified Data.Text.IO as T
 
 -- | It verify that the answer provided is verified by itself
 main :: IO ()
 main = forM_ (subs mod0 ++ subs mod1 ++ subs mod2 ++ subs mod3 ++ subs mod4) $ \submod -> do
   let ans = fullAnswer submod
   res <- evalWithAns ans (T.unpack $ answer ans)
-  either (\e -> die $ "Non Coherent\n" ++ show submod ++ show e) (const $ return ()) res
+  either (\e -> print submod >> T.putStrLn e >> die "") (const $ return ()) res
